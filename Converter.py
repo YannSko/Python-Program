e0a41d88a8485ef3b5ec83d9f1e29683578560d0
YannSko <101813005+YannSko@users.noreply.github.com>
2022-11-10 10:59:53 +0100

    Converter.py

---
+++ Converter.py
@@ -0,0 +1,26 @@
+# Crée des fonctions capable de convertir un integer en string, un integer en float et un float en string
+b = int(input())
+
+
+def Int_To_String(Number):
+
+    print("J'ai"+str(b)+"ans")
+
+
+Int_To_String(b)
+
+
+def Int_to_Float(Number2):
+    print(float(b))
+    return b
+
+
+Int_to_Float(b)
+
+
+def Float_to_String(b):
+    c = str(b)
+    print("jai"+c+"ans")
+
+
+Float_to_String(b)
