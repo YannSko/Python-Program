1109465c48b1c35e59a0fe1d0fa0454e1ea892a0
YannSko <101813005+YannSko@users.noreply.github.com>
2022-11-10 13:49:14 +0100

    Calculator
    calculate some int

---
+++ Calculator.py
@@ -0,0 +1,30 @@
+def subtract(a, b):
+    Result = a-b
+    return Result
+
+
+def Div(a, b):
+    Result1 = a/b
+    return Result1
+
+
+def Pourcent(a, b):
+    Result = (a*b)/100
+
+    return Result
+
+
+def main():
+
+    a = int(input())
+    b = int(input())
+
+    print(Pourcent(a, b))
+
+
+def Sum(a, b):
+    Result = a+b
+    return Result
+
+
+main()
