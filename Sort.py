0f7a127ae53176f9c346d9bab16b1476e949be89
YannSko <101813005+YannSko@users.noreply.github.com>
2022-11-10 11:34:34 +0100

    Sort.py
    Sort a int List by Cr order

---
+++ Sort.py
@@ -0,0 +1,24 @@
+# Crée une fonction Liste qui prends 1 paramètres
+# "L" , L étant la liste ci dessous :
+#L = [0,6,4,2,10]
+# Je veux que tu me return la liste par ordre croissant soit :
+#L = [0,2,4,6,10]
+# ATTENTION : Je veux que tu trie la liste avec un algorithme et pas manuellement !
+# tips : regarder les algorithme de tri en python
+L = [0, 6, 4, 2, 10]
+
+
+def Sort(L):
+    c = 0
+    print(len(L))
+    for i in range(0, len(L)-1):
+        if L[i] > L[i+1]:
+            c = L[i]
+            L[i] = L[i+1]
+            L[i+1] = c
+        if L[i] < L[i+1]:
+            continue
+    print(L)
+
+
+Sort(L)
