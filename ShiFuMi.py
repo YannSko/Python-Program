f84e0b32d9895c73fda7c61b6b53ef65abad16cd
YannSko <101813005+YannSko@users.noreply.github.com>
2022-11-19 17:46:08 +0100

    Update ShiFuMi.py

--- ShiFuMi.py
+++ ShiFuMi.py
@@ -2,3 +2,33 @@ import random
 
 
 def ShiFuMi():
+
+    Possibility = ["ciseaux", "pierre", "papier"]
+    Player = input("Choisissez parmi pierre, papier, ciseaux \n")
+    print(" Pierre Feuille Ciseaux !")
+    Opponent = random.randint(0, 3)
+    Opponent = Possibility[Opponent]
+    print(" L'ordinateur a fait" + " " + Opponent)
+    if(Player == "pierre"):
+        if(Opponent == "papier"):
+            print("Perdu!", Opponent, "recouvre", Player)
+        else:
+            print("Gagné!", Player, "écrase", Opponent)
+
+    elif(Player == "papier"):
+        if(Opponent == "ciseaux"):
+            print("Perdu!", Opponent, "cut", Player)
+
+        else:
+            print("You win!", Player, "recouvre", Opponent)
+    elif(Player == "ciseaux"):
+        if(Opponent == "Rock"):
+            print("Perdu...", Opponent, "écrase", Player)
+        else:
+            print("Gagné!", Player, "cut", Opponent)
+
+    if(Player == Opponent):
+        print("Egalité!")
+
+
+ShiFuMi()
