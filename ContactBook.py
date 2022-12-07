e660c5f1a9514e763700c5ff8fb406603c70200f
YannSko <101813005+YannSko@users.noreply.github.com>
2022-12-07 14:55:09 +0100

    Create ContactBook.py

---
+++ ContactBook.py
@@ -0,0 +1,29 @@
+#his is one of the excellent python projects for beginners. 
+# Everyone uses a contact book to save contact details, including name, address, phone number, and even email address. 
+# The main objective of this project is to generate a contact book using python where users can add a new contact, edit, or delete existing contacts and view the details of all their contacts. 
+# This is one of the coolest project ideas in python for beginners to help strengthen their command of the programming language.
+
+class Personne:
+    def __init__(self,name,surname,phonenumber,address,email):
+        self.name=name
+        self.surname=surname
+        self.phonenumber=phonenumber
+        self.address=address 
+        self.email=email
+        self.address=address
+
+bob=Personne("Bob","Joe","06084564","6blvd Moulin","@mail.com")
+
+Dictionnaire={}
+Dictionnaire['Bob']=bob
+
+
+
+
+
+
+
+    
+    
+
+    
