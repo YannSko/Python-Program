c4b82d35db7cd0b06d8e5c6b60d6e43792c3e7e3
YannSko <101813005+YannSko@users.noreply.github.com>
2022-12-07 16:11:30 +0100

    Guess

--- GuessingGameOrdi.py
+++ GuessingGameOrdi.py
@@ -1,27 +1,34 @@
 import random
 
 
-def Guess():
-    Guess_Number = random.randint(0, 100)
+def CompGuess():
+    Guess_Number = int(input("Lets find the good number entre 0 et 100 \n")) 
     i = False
     compt=0
     print(Guess_Number)
     while i ==False:
         
-        Try = int(input("Lets find the good number \n"))
+        Try = random.randint(0, 100)
         compt +=1
         if compt ==7:
             i=True
             print("Fin de la partie mec")
         elif Try < Guess_Number:
             print("You are too Low")
+            print(Try)
+            Try2 = random.randint(Try,100)
+            Try=Try2
         elif Try > Guess_Number:
             print("You are too High")
+            print(Try)
+            Try2 = random.randint(0,Try)
+            Try=Try2
         elif Try == Guess_Number:
+            print(Try)
             print("You Guessed the number in "+str(compt)+"tries")
            
             
         
 
 
-Guess()
+CompGuess()
