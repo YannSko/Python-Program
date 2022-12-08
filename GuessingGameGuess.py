cc36d25308d6027affc2ad18031fb60d687bba5d
YannSko <101813005+YannSko@users.noreply.github.com>
2022-12-08 11:11:09 +0100

    Create GuessingGameGuess.py

---
+++ GuessingGameGuess.py
@@ -0,0 +1,15 @@
+import random
+
+def Guess():
+    print("the ordi will find your number")
+    testnumb=50
+    i=True
+    while i==True:
+        print("is it " +str(testnumb)+"?"'\n')
+        test=input("low or high")
+        if test =="low":
+            testnumb =testnumb*1.5
+        elif test == "high":
+            testnumb = testnumb*0.5
+
+Guess()
