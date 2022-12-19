59c029aed87787243c68f17d16cd593da60f2d7c
YannSko <101813005+YannSko@users.noreply.github.com>
2022-12-19 14:40:16 +0100

    Create GetMin_Max_Word.py

---
+++ GetMin_Max_Word.py
@@ -0,0 +1,10 @@
+#Find the shorter and the longer word in a sentence
+
+
+def GetMin_Max_Word(s):
+    return 0
+
+s=" La pomme tombe de l' arbre"
+min_word, max_word = GetMin_Max_Word(s)
+print("Le mot le plus petit est",min_word)
+print("Le mot le plus grand est",max_word)
