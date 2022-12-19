6ec6b50dc9b42044ada3738f07ad4ed89bffbdb8
YannSko <101813005+YannSko@users.noreply.github.com>
2022-12-19 14:55:22 +0100

    Update GetMin_Max_Word.py

--- GetMin_Max_Word.py
+++ GetMin_Max_Word.py
@@ -2,12 +2,20 @@
 
 
 def GetMin_Max_Word(s):
-    words=s.split(" ")
+    words = s.split(" ")
     min_word = min(words, key=len)
     max_word = max(words, key=len)
     return(min_word, max_word)
 
-s=" La pomme tombe de l'arbre"
+#find by alphabetic order
+def GetMin_Max_Word2(s):
+    words = s.split(" ")
+    words.sort()
+    min_word = min(words, key=len)
+    max_word = max(words, key=len)
+    return min_word, max_word
+s=" la moto roule aussi vite que le camion"
 min_word, max_word = GetMin_Max_Word(s)
+min_word, max_word = GetMin_Max_Word2(s)
 print("Le mot le plus petit est",min_word)
 print("Le mot le plus grand est",max_word)
