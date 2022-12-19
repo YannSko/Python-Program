13dfcbd179dd3ec8a9af5ad7d6aefc986495d6ca
YannSko <101813005+YannSko@users.noreply.github.com>
2022-12-19 17:07:05 +0100

    Update FindMissingElements.py

--- FindMissingElements.py
+++ FindMissingElements.py
@@ -4,6 +4,9 @@
 l=[3,8,4,7,2]
 
 def Get_Missing_Numbers(l,min,max):
-    return[]
-
+    missing =[]
+    for i in range(min,max+1):
+        if i not in l:
+            missing.append(i)
+    return missing
 print(Get_Missing_Numbers(l,1,10))
