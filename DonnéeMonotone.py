1b475a52119146fe0b8b0aa83d8d3f974b372afb
YannSko <101813005+YannSko@users.noreply.github.com>
2022-12-19 15:37:45 +0100

    Update DonnéeMonotone.py

--- DonnéeMonotone.py
+++ DonnéeMonotone.py
@@ -5,4 +5,10 @@ a = [1,2,3,4]
 b = [1,2,1,0]
 
 def IsMonotonicValue(l):
-    return 0
+    for i in range(len(l)-1):
+        if l[i+1] < l[i]:
+            return False
+    return True
+
+print("a:", IsMonotonicValue(a))
+print("b:", IsMonotonicValue(b))
