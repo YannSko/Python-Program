175b2831969f923e2399f2f1d92765c7f253ae44
YannSko <101813005+YannSko@users.noreply.github.com>
2022-12-19 15:07:56 +0100

    Update CommonWord.py

--- CommonWord.py
+++ CommonWord.py
@@ -1,5 +1,18 @@
 # Find the common words between 2 sentences
 
 
-a = "My name is Yann"
+a = "My Name is Yann"
 b = "Your name is Yanno"
+
+def CommonWord(a, b):
+    words_a = a.lower().split(" ")
+    words_b = b.lower().split(" ")
+
+    common_words = []
+
+    for word in words_a:
+        if word in words_b:
+            common_words.append(word)
+    return common_words
+
+print(CommonWord(a, b ))
