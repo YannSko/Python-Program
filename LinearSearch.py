e7e99701f2e5dc68cb41ead8cb94dcad90609050
YannSko <101813005+YannSko@users.noreply.github.com>
2022-12-21 13:42:55 +0100

    Update LinearSearch.py

--- LinearSearch.py
+++ LinearSearch.py
@@ -1,3 +1,9 @@
 #linear search(l,e)
 #--> index first occurence
 # --> -1: if nothing found
+
+def linear_search(l,e):
+    for i in range(len(l)):
+        if l[i]==e:
+            return i
+    return -1
