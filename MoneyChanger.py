3a34b10c1989e245cf5a68ccc2a8446586ef3545
YannSko <101813005+YannSko@users.noreply.github.com>
2022-12-23 10:23:19 +0100

    Update MoneyChanger.py

--- MoneyChanger.py
+++ MoneyChanger.py
@@ -1,1 +1,19 @@
 #make an ATM
+
+def cash_back_greedy(s, e):
+    r = []
+    for a in e:
+        n = s//a
+        r.append(n)
+        s -= n*a
+
+    for i in range(len(e)):
+        if r[i] > 0:
+            print(e[i], "x", r[i])
+
+    if s > 0:
+        print("Reste de :", s)
+    else:
+        print("Toute la monnaie a été rendue")
+
+    return r
