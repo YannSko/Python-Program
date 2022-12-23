69be69cb6083b5a9b2346d3ef855f4f0dbb90498
YannSko <101813005+YannSko@users.noreply.github.com>
2022-12-23 13:45:35 +0100

    Erreur Check +get some accurate data

--- BitCoinAnalyser/main.py
+++ BitCoinAnalyser/main.py
@@ -10,4 +10,21 @@ response = requests.get(url, headers=headers)
 
 data = json.loads(response.text)
 
-print()
+# 200 / sinon afficher le code d'erreur
+if response.status_code == 200:
+    print("L'appel à l'API a fonctionné")
+    data = json.loads(response.text)
+    nb_assets = len(data)
+    # asset_id
+    # name
+    print("Nombre d'assets monétaires:", nb_assets)
+    if nb_assets >= 10:
+        for i in range(10):
+            d = data[i]
+            print(d["asset_id"] + ": " + d["name"])
+
+    print()
+else:
+    # cas d'erreur
+    print("L'appel à l'API a retourné une erreur:", response.status_code)
+
